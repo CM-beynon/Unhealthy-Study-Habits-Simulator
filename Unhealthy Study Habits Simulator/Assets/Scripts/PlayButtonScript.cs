29668338156da0288939c3a6f12556a8c694ff4a
John Manning <john.manning144@gmail.com>
2021-01-09 13:53:40 -0500

    Added button that changes to sample scene
    Added Script File, new scene, Button

---
+++ Unhealthy Study Habits Simulator/Assets/Scripts/PlayButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonScript : MonoBehaviour {
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Start");
+        Button btn = GameObject.Find("PlayGameButton").GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+        SceneManager.LoadScene("SampleScene");
+	}
+}
+
+
