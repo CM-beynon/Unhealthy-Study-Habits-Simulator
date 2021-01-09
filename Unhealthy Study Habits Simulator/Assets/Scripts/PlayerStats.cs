2df3bc683f2f142952d1526fe9b908ed19f8f3fa
Ceilidh Simon <43501977+LeeSimon77@users.noreply.github.com>
2021-01-09 15:41:13 -0600

    Outline and examples for ActionButtons
    added an ExampleButton prefab and an actionExample script to demo how AAction works

--- Unhealthy Study Habits Simulator/Assets/Scripts/PlayerStats.cs
+++ Unhealthy Study Habits Simulator/Assets/Scripts/PlayerStats.cs
@@ -54,6 +54,8 @@ public class PlayerStats : MonoBehaviour
 
     public void incHunger(int inc)
     {
+        Debug.Log("Hunger increased");
+        Debug.Log(inc);
         hunger += inc;
     } // end incHunger
 
