5af49293315bd342eed1008e0b37a1e32e61da43
Mark Robitaille <mark.rj.robitaille@gmail.com>
2021-01-10 01:36:56 -0600

    Added pausing visuals as prefab

--- Unhealthy Study Habits Simulator/Assets/Scripts/PauseButtonScript.cs
+++ Unhealthy Study Habits Simulator/Assets/Scripts/PauseButtonScript.cs
@@ -6,17 +6,40 @@ using UnityEngine.UI;
 public class PauseButtonScript : MonoBehaviour
 {
     public PlayerStats player;
+    public bool textVisible = false;
+    public double nextTextVisibleToggle;
+    public GameObject pauseText;
+    public double textInterval = 0.5;
 
     // Start is called before the first frame update
     void Start()
     {
+        nextTextVisibleToggle = Time.time + textInterval;
         Button btn = GameObject.Find("PauseButton").GetComponent<Button>();
         btn.onClick.AddListener(TaskOnClick);
     }
 
+    void Update()
+    {
+        if ((player.getPaused() && Time.time >= nextTextVisibleToggle) || (!player.getPaused() && textVisible))
+        {
+            textVisible = !textVisible;
+            nextTextVisibleToggle += textInterval;
+            pauseText.SetActive(textVisible);   
+        }
+    }
+
     private void TaskOnClick()
     {
-        Debug.Log("Toggled Pause");
+        if (player.getPaused()) // unpause
+        {
+            Debug.Log("Unpause");
+            //textVisible = false;
+        } else // pause
+        {
+            Debug.Log("Pause");
+            nextTextVisibleToggle = Time.time;
+        }
         player.togglePause();
     }
 }
