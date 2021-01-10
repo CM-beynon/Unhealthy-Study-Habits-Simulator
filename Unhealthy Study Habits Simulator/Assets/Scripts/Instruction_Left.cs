dceb1ebcb5a58f76c68558f283f94284b077478a
icybreeze9 <tj_bautista11@yahoo.com>
2021-01-10 16:57:43 -0600

    Reformed Instruction Scene

---
+++ Unhealthy Study Habits Simulator/Assets/Scripts/Instruction_Left.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instruction_Left : MonoBehaviour
+{
+    public GameObject page1;
+    public GameObject page2;
+    public GameObject page3;
+    public GameObject rightArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button left = GameObject.Find("Left").GetComponent<Button>();
+        left.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        page1.SetActive(true);
+        page2.SetActive(true);
+        page3.SetActive(false);
+        //this.gameObject.SetActive(false);
+        rightArrow.SetActive(true);
+    }
+}
---
+++ Unhealthy Study Habits Simulator/Assets/Scripts/Instruction_Right.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instruction_Right : MonoBehaviour
+{
+    public GameObject page1;
+    public GameObject page2;
+    public GameObject page3;
+    public GameObject leftArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button right = GameObject.Find("Right").GetComponent<Button>();
+        right.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        page1.SetActive(false);
+        page2.SetActive(false);
+        page3.SetActive(true);
+        //this.gameObject.SetActive(false);
+        leftArrow.SetActive(true);
+    }
+}
