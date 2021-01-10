1abbb55eb48fb1cc9321defb10f454cc0fa07237
Mark Robitaille <mark.rj.robitaille@gmail.com>
2021-01-09 23:41:27 -0600

    tiny pause button script name change before merging

--- Unhealthy Study Habits Simulator/Assets/Scripts/PauseButtonScript.cs
+++ Unhealthy Study Habits Simulator/Assets/Scripts/PauseButtonScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PauseButton : MonoBehaviour
+public class PauseButtonScript : MonoBehaviour
 {
     public PlayerStats player;
 
