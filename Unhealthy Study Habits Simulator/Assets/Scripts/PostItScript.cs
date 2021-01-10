225f49395cc5a0e08b8ee3602f9e08b080959bfd
Mark Robitaille <mark.rj.robitaille@gmail.com>
2021-01-09 22:35:25 -0600

    Created Post-It Note Prefab

---
+++ Unhealthy Study Habits Simulator/Assets/Scripts/PostItScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PostItScript : MonoBehaviour
+{
+    public Clock clock;
+    public Text textBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = "Final\nExam\nat\n" + clock.formatTime(clock.examHour, 0, false);
+    }
+}
