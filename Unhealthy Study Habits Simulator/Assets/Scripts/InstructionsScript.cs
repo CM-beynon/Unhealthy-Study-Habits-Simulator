fb2df9cb330bcad63c01ab69b31ef487e499de97
Ceilidh Simon <43501977+LeeSimon77@users.noreply.github.com>
2021-01-10 13:27:33 -0600

    Instructions Page
    Added instructions page and navigation to and from it

---
+++ Unhealthy Study Habits Simulator/Assets/Scripts/InstructionsScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InstructionsScript : MonoBehaviour
+{
+
+    public AudioSource playingMusic;
+    public AudioSource soundEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Start");
+        Button btn = GameObject.Find("InstructionsButton").GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        StartCoroutine(RestartGame());
+    }
+
+    IEnumerator RestartGame()
+    {
+        Debug.Log("Going to Instructions");
+        playingMusic.Pause();
+        soundEffect.Play();
+        yield return new WaitForSeconds(soundEffect.clip.length);
+        SceneManager.LoadScene("InstructionsScene");
+    }
+}
